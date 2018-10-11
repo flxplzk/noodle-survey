053b9336209a1c766be20229ad75aaa6fc547a58
Felix Plazek (U555994) <felix.plazek@lhind.dlh.de>
2018-10-11 15:18:11 +0200

    rest resources secured with basic auth, using userservice for authetication

---
+++ src/test/java/de/nordakademie/iaa/examsurvey/service/impl/UserServiceImplTest.java
@@ -0,0 +1,26 @@
+package de.nordakademie.iaa.examsurvey.service.impl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserServiceImplTest {
+
+    @Test
+    public void save() {
+        // GIVEN
+
+        // WHEN
+
+        // THEN
+    }
+
+    @Test
+    public void loadUserByUsername() {
+        // GIVEN
+
+        // WHEN
+
+        // THEN
+    }
+}
