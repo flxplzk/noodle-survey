4af39cf5895261668f913de816576d33cbecb48f
Felix Plazek (U555994) <felix.plazek@lhind.dlh.de>
2018-10-11 16:25:05 +0200

    added login and register pages

--- src/main/webapp/js/modules/app.module.js
+++ src/main/webapp/js/modules/app.module.js
@@ -1,11 +1,26 @@
 (function() {
     var app = angular.module("de.nordakademie.iaa.survey", [
         "ngRoute",
-        "ngResource",
         "rx"
     ]);
 
     app.controller("mainController", ["$rootScope", function ($rootScope) {
        $rootScope.greeting = "Hallo Welt";
     }]);
+
+    app.config(function ($routeProvider) {
+        $routeProvider
+
+            .when("/login", {
+                templateUrl: "js/components/authentication/login.template.html"
+            })
+
+            .when("/register", {
+                templateUrl: "js/components/authentication/register.template.html"
+            })
+
+            .otherwise({
+                templateUrl: "js/components/authentication/login.template.html"
+            })
+    })
 }());
